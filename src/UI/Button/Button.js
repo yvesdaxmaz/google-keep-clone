1b33de7de4f257036f2f5a0150e36c51133749b3
Yves Kipondo <yvesdaxmaz@gmail.com>
2020-07-18 13:51:39 +0200

    add click handler to button tag on Button component

--- src/UI/Button/Button.js
+++ src/UI/Button/Button.js
@@ -3,25 +3,23 @@ import React from 'react';
 const Button = ({ title, children, classes, clicked, texted, small }) => {
   if (texted) {
     return (
-      <div
+      <button
         className={`flex flex-shrink-0 text-gray-800 font-bold items-center px-4 py-1 hover:bg-gray-200 ${classes}`}
+        onClick={clicked}
       >
-        <button type="button" title={title} onClick={clicked}>
-          {title}
-        </button>
-      </div>
+        {title}
+      </button>
     );
   } else {
     return (
-      <div
+      <button
         className={`flex flex-shrink-0 items-center justify-center ${
           small ? 'h-8 w-8' : 'h-10 w-10'
         } rounded-full hover:bg-gray-200 ${classes}`}
+        onClick={clicked}
       >
-        <button type="button" title={title} onClick={clicked}>
-          {children}
-        </button>
-      </div>
+        {children}
+      </button>
     );
   }
 };
