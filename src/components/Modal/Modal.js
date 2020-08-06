48afb2df5d522eb76a64b43fabe11003d4ffa728
Yves Kipondo <yvesdaxmaz@gmail.com>
2020-08-06 21:11:01 +0200

    hide modal when clicked overlay

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,16 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import Button from './../../UI/Button/Button';
 import Label from './../../UI/Label/Label';
 import KeepContext from './../../context/KeepContext';
 
 const Modal = ({ classes }) => {
   const { labels, endEdit } = useContext(KeepContext);
+  const wrapperRef = useRef(null);
+
+  const handleMouseDown = event => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      endEdit();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousedown', handleMouseDown);
+    return () => {
+      window.removeEventListener('mousedown', () => {});
+    };
+  }, []);
 
   return (
     <div
       className={`fixed flex items-center justify-center z-20 top-0 left-0 h-full w-full bg-gray-800 bg-opacity-50`}
     >
-      <div className="w-full max-w-sm bg-white border shadow text-gray-700">
+      <div
+        ref={wrapperRef}
+        className="w-full max-w-sm bg-white border shadow text-gray-700"
+      >
         <div className="py-4 px-4">
           <h2 className="text-xl">Modifier les libellés</h2>
         </div>
